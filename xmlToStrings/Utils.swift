a83bf0977196b10d926c992db46d537bc9733cc7
LSO <lso@frontware.co.th>
2016-08-19 10:55:42 +0700

    handle subsitute end of line

--- xmlToStrings/Utils.swift
+++ xmlToStrings/Utils.swift
@@ -11,13 +11,18 @@ import Foundation
 func escapeDoubleQuote(strWith2Quote:String) -> String {
     var res:String = String("")
     var inSubsitute:Bool = false
+    var nbCaract:Int = 0
     for c in strWith2Quote.characters {
+        nbCaract += 1
         if inSubsitute {
             if c == " " {
                 res = res + "@"
                 inSubsitute = false
             } else if c == "d" {
                 inSubsitute = false
+            } else if  nbCaract == strWith2Quote.characters.count {
+                res = res + "@"
+                continue
             } else {
                 continue
             }
